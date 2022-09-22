336cafc7271a4dbd0a291730476997a93cafac96
Jhosse Paul Marquez Ruiz <mcromanceva@outlook.com>
2022-09-21 19:02:26 -0500

    Changes config to use the subdirectory docs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig(configEnv => ({
+  build: {
+    outDir: './docs'
+  },
   plugins: [
     react(),
     tsConfigPaths(),
