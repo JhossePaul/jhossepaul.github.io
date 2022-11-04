5a2b121323710ed7055d511c5c3e32b9e17bec0c
Jhosse Paul Marquez Ruiz <mcromanceva@outlook.com>
2022-11-04 10:50:44 -0600

    Partial update

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -10,18 +10,18 @@ export default function Portfolio () {
       <SectionContent>
         <ul>
           <li>
-            <a href="https://github.com/JhossePaul/Portable-Analytical-Environment">
+            <a href='https://github.com/JhossePaul/Portable-Analytical-Environment'>
               A analytical portable environment with R backend
             </a>
           </li>
           <li>
-            <a href="https://github.com/JhossePaul/AzureClient">
+            <a href='https://github.com/JhossePaul/AzureClient'>
               A R package to consume Azure Blob Storage API
             </a>
           </li>
           <li>
-            <a href="">
-            A C++ package with a R interface to validate loan portfolio payments
+            <a href='https://github.com/JhossePaul/ValidacionCredito'>
+              A C++ package with a R interface to validate loan portfolio payments
             </a>
           </li>
         </ul>
